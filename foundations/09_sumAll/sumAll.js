7d3be44a48daf964892c4330e2c5a77c1a805e81
Elias Mir <elias.mir2004@gmail.com>
2026-03-05 13:31:51 +0100

    loops and arrays chapter done

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(from, to) {
+    if (typeof from !== 'number' || typeof to !== 'number' || from < 0 || to < 0 || !Number.isInteger(from) || !Number.isInteger(to)) {
+        return 'ERROR';
+    }
+    let sum = 0
+    const min = Math.min(from, to);
+    const max = Math.max(from, to);
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
